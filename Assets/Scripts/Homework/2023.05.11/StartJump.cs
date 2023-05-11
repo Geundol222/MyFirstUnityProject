7f2c46f844c6a4ec2d6cb09d0f8c901939b56e00
Geundol222 <geundol222@gmail.com>
2023-05-11 21:17:01 +0900

    feat : 2023.05.11 Homework

---
+++ Assets/Scripts/Homework/2023.05.11/StartJump.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartJump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    [SerializeField]
+    private int jumpForce = 5;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
