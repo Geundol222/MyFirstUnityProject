6a24954f1d3db139625ec18d688635ec2ca9adb4
Geundol222 <geundol222@gmail.com>
2023-05-11 21:48:29 +0900

    docs : HomeWork 주석 추가

--- Assets/Scripts/Homework/2023.05.11/ChangeName.cs
+++ Assets/Scripts/Homework/2023.05.11/ChangeName.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ChangeName : MonoBehaviour
 {
-    public GameObject gameObject;
+    public GameObject gameObject;       // 게임 오브젝트를 저장할 변수 gameObject를 만들어주고 public으로 선언하여 인스펙터창에서 오브젝트를 넣어줄 수 있게 한다.
 
     private void Awake()
     {
-        gameObject.name = "Player";
+        gameObject.name = "Player";     // 게임이 처음 초기화 될때 Player로 이름을 바꿔준다.
     }
 }
--- Assets/Scripts/Homework/2023.05.11/StartJump.cs
+++ Assets/Scripts/Homework/2023.05.11/StartJump.cs
@@ -7,15 +7,15 @@ public class StartJump : MonoBehaviour
     Rigidbody rigidbody;
 
     [SerializeField]
-    private int jumpForce = 5;
+    private int jumpForce = 5;              // 점프에 부여할 힘을 저장할 변수를 선언하고 어트리뷰트를 지정하여 인스펙터상에서 변경이 가능하게 한다.
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();          // rigidbody에 Rigidbody 컴포넌트를 저장하여 초기화한다.
     }
 
     private void Start()
     {
-        rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  // 게임이 시작되면 Rigidbody컴포넌트에 jumpForce 만큼의 힘을 위로 부여한다.
     }
 }
